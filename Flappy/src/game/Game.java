8870ed8e550405094ed4d2b157fcbb6c43bd0bbc
Mozar <Mozar@DESKTOP-44333LK>
2019-02-23 12:01:42 +0100

    menu loop done

--- Flappy/src/game/Game.java
+++ Flappy/src/game/Game.java
@@ -13,49 +13,54 @@ import java.awt.Graphics2D;
 
 
 public class Game implements Runnable{
-	
+
 	public Thread activity = new Thread(this);
 	public final static int WIDTH = 800, HEIGHT = 600;
-			
+
 	private String gameName = "Flaccidbird";
-	
+
 	private Canvas game = new Canvas();
-	
+
 	private Input input;
-	
+
 	private ArrayList<Updatable> updatables = new ArrayList<>();
 	private ArrayList<Renderable> renderables = new ArrayList<>();
-	
+
 	JFrame menu;
-	
-	public Game(JFrame menu) {
-		this.menu = menu;
+	Menu test;
+
+	public Game(Menu test) {
+		this.test = test;
+		this.menu = test.getFrame();
 	}
-	
+
 	public void addUpdatable(Updatable u) {
 		updatables.add(u);
 	}
-	
+
 	public void removeUpdatable(Updatable u) {
 		updatables.remove(u);
 	}
-	
+
 	public void addRenderable(Renderable r) {
 		renderables.add(r);
 	}
-	
+
 	public void removeRenderable(Renderable r) {
 		renderables.remove(r);
 	}
-	
+
 	@Override
 	public void run() {
 		//TODO
 		start();	
 	}
-	
+
+	//Behöver förkorta den här funktionen
 	private void start() {
 		Dimension gameSize = new Dimension(Game.WIDTH, Game.HEIGHT); 
+		
+		
 		game.setSize(gameSize);
 		game.setMinimumSize(gameSize);
 		game.setMaximumSize(gameSize);
@@ -65,35 +70,35 @@ public class Game implements Runnable{
 		//Init input
 		input = new Input();
 		game.addKeyListener(input);
-		
+
 		//Game loop
 		final int TICKS_PER_SECOND = 60; //one update of the game loop, 60fps
 		final int TIME_PER_TICK = 1000 / TICKS_PER_SECOND; //in milliseconds
 		final int MAX_FRAMESKIPS = 5; //max amount of updates per render
-		
+
 		long nextGameTick = System.currentTimeMillis(); //lets us make time-based calculations in-game
 		int loops;
 		float interpolation; //gives number of times we have updated for each render, sync objects with rendered frame
-		
+
 		long timeAtLastFPSCheck = 0; //check frames per second, each second
 		int ticks = 0;
-			
+
 		while (true) {
 			//Updating
 			loops = 0; //max 5, after 5 game has to be rendered
-			
+
 			while (System.currentTimeMillis() > nextGameTick && loops < MAX_FRAMESKIPS) {
-				 update(); //loops through every updatable object
-				 ticks++; //update means a game tick
-				 nextGameTick+= TIME_PER_TICK; //Predicted time, gives time the next tick SHOULD be done by
-				 loops++; 
+				update(); //loops through every updatable object
+				ticks++; //update means a game tick
+				nextGameTick+= TIME_PER_TICK; //Predicted time, gives time the next tick SHOULD be done by
+				loops++; 
 			}
-			
+
 			//Rendering
 			interpolation = (float) (System.currentTimeMillis() + TIME_PER_TICK - nextGameTick) //comparison between actual time and predicted time
-							/ (float) TIME_PER_TICK; //i.e. if computer is slow
+					/ (float) TIME_PER_TICK; //i.e. if computer is slow
 			render(interpolation);
-			
+
 			//FPS Check
 			if (System.currentTimeMillis() - timeAtLastFPSCheck >= 1000) {
 				System.out.println("FPS: " + ticks);
@@ -101,58 +106,59 @@ public class Game implements Runnable{
 				ticks = 0; //reset FPS every second
 				timeAtLastFPSCheck = System.currentTimeMillis();
 			}
-			
+
 			if(Thread.interrupted()) {
-				//inte vill spela igen
-				//starta upp hela spelet igen
 				if(!playAgain()) {
-					//eller återvända till menu?
-					
-					System.exit(0);
+					menu.getContentPane().removeAll();
+					test.testMenu();
+					break;
+
 				}	
 			}	
 		}
 	}
-	
-	//ska vi sätta highscore name här?
-	//eller i början när man startar spelet
-	//om inget namn sätts så blir namnet något Default
-	//test guest_xxxxxx
-	private boolean playAgain()
-	{
-		int value = JOptionPane.showConfirmDialog(null,
-				 "Play again?", "GAME OVER ", JOptionPane.YES_NO_OPTION,
-				 JOptionPane.QUESTION_MESSAGE,
-				 new ImageIcon(this.getClass().getResource("/start.jpg"))); 
-		
-		if(value == 0)
-			return true;
-		else
-			return false;	
-	}
-	
+
+
+
+
 	private void update() {
 		for(Updatable u : updatables) {
 			u.update(input);
-			}
 		}
-	
+	}
+
 	private void render(float interpolation) {  //renders all game objects
-		
+
 		BufferStrategy b = game.getBufferStrategy();
 		if (b == null) {
 			game.createBufferStrategy(2); //create double buffer to insure user doesn't see screen getting cleared and re-rendered
 			return; //double buffer ensures there is a frame shown when screen is cleared
 		}
-		
+
 		Graphics2D g =(Graphics2D) b.getDrawGraphics();
 		g.clearRect(0, 0, game.getWidth(), game.getHeight());
-		
+
 		for(Renderable r : renderables) {
 			r.render(g, interpolation);
-			}
+		}
 		g.dispose();
 		b.show();
-		}
+	}
+
+
+	private boolean playAgain()
+	{
+		int value = JOptionPane.showConfirmDialog(null,
+				"Play again?", "GAME OVER ", JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				new ImageIcon(this.getClass().getResource("/start.jpg"))); 
+
+		if(value == 0)
+			return true;
+		else
+			return false;	
+	}
+
+
+
 }
- 
--- Flappy/src/game/Menu.java
+++ Flappy/src/game/Menu.java
@@ -1,5 +1,4 @@
 package game;
-import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -13,87 +12,60 @@ public class Menu implements Observer {
 	private String gameName = "Flaccidbird";
 	private Game g;
 	private JFrame menu;
+	private JPanel panel;
+	private JPanel save;
+
+
 	public Menu() {
 		makeMenu();
-		init();
+		//init();
 	}
 
-	
+
 	//fråga  om JFrame om det är bättre om olika öppnas eller inte 
 	//det blir mindre kohesion om olika frames används
 	//då behöver jag bara använda setVisible
 	//och behöver inte återvända till Main eller?
 	private void makeMenu() {
-		
-/*<<<<<<< HEAD
-	menu = new JFrame();
-	menu.setLayout(new GridLayout(2,1));
-	JButton startButton = new JButton("Start");
-	startButton.addActionListener(e -> {
-		new Thread(new Start(new JFrame())).start();
-	});
-	menu.add(startButton);
-	JButton highScore = new JButton("Highscore");
-	highScore.addActionListener(e -> {
-		// Skriv här
-		new Thread(new Highscores()).start();
-	});
-	menu.add(highScore);
-	menu.pack();
-	menu.setDefaultCloseOperation(1);
-	menu.setVisible(true);
-=======*/
 		Dimension gameSize = new Dimension(WIDTH, HEIGHT); 
 		menu = new JFrame();
-		
 		menu.setTitle(gameName);
 		menu.setSize(gameSize); 
-		menu.setResizable(false);
-		
-		g = new Game(menu);
-		menu.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;
-
-        
+		//menu.setResizable(false);
+		panel = new JPanel();
+		panel.setSize(gameSize);
+		panel.setLayout(new GridLayout(2,1));
 		JButton startButton = new JButton("Start ",new ImageIcon(this.getClass().getResource("/start.jpg")));
 		startButton.addActionListener(e -> {
-			menu.getContentPane().removeAll();
+			g = new Game(this);
+			init();
+			menu.getContentPane().removeAll();	
 			g.activity.start();	
 		});
 
-		c.ipady = 300;      //make this component tall
-		c.ipadx = 800;      //make it width
-		c.weightx = 0.0;
-		c.gridwidth = 0;
-		c.gridx = 0;
-		c.gridy = 0;
-		menu.add(startButton,c);
-
+		panel.add(startButton);
 		JButton HS_off = new JButton("HIGHSCORE ",new ImageIcon(this.getClass().getResource("/photo.jpg")));
-		
-
 		HS_off.addActionListener(e -> {
-			
 			new Thread(new Highscores()).start();
-			
 		});
-		
-		c.ipady = 300;      //make this component tall
-		c.ipadx = 800;      //make it width
-		c.weightx = 0.0;
-		c.gridwidth = 0;
-		c.gridx = 0;
-		c.gridy = 1;
-		
-		
-		menu.add(HS_off,c);
+
+		panel.add(HS_off);
+		menu.add(panel);
 		menu.pack();
 		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		menu.setSize(800, 600);
 		menu.setVisible(true);	
 	}
 
+
+	public void testMenu(){
+		menu.add(panel);
+	}	
+
+
+	public JFrame getFrame() {
+		return menu;
+	}
 
 	private void init() {
 		// Initialise game objects
@@ -110,15 +82,17 @@ public class Menu implements Observer {
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		
+
 		if(arg0 instanceof Bird && arg1 instanceof Integer) {
 			//Sam du kanske kan använda denna score för server
 			int LastScore = (Integer) arg1;
+
+			System.out.println("Test: Lastscore was " + LastScore+ "\n");
+
 			//interrupt game Loop
 			g.activity.interrupt();
 		}
 	}
-//>>>>>>> branch 'master' of https://github.com/banuunuu/Flappy.git
 }
 
 
